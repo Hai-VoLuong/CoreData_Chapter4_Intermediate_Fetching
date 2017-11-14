e1e2afa56b1692bfd904b83723d60b2ec86ebaa9
HaiVL <hai.softech@gmail.com>
2017-11-14 16:41:58 +0700

    feature - get data and display reference data to fifterViewController

--- Bubble Tea Finder/FilterViewController.swift
+++ Bubble Tea Finder/FilterViewController.swift
@@ -21,6 +21,7 @@
  */
 
 import UIKit
+import CoreData
 
 class FilterViewController: UITableViewController {
 
@@ -45,16 +46,32 @@ class FilterViewController: UITableViewController {
   @IBOutlet weak var distanceSortCell: UITableViewCell!
   @IBOutlet weak var priceSortCell: UITableViewCell!
 
+  //MARK: - Properties
+  var coreDataStack: CoreDataStack!
+  lazy var cheapVenuePredicate: NSPredicate = {
+    return NSPredicate(format: "%K == %@", #keyPath(Venue.priceInfo.priceCategory), "$")
+  }()
+
+  lazy var moderateVenuePredicate: NSPredicate = {
+    return NSPredicate(format: "%K == %@", #keyPath(Venue.priceInfo.priceCategory), "$$")
+  }()
+
+  lazy var expensiveVenuePredicate: NSPredicate = {
+    return NSPredicate(format: "%K == %@", #keyPath(Venue.priceInfo.priceCategory), "$$$")
+  }()
+
   // MARK: - View Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    populateCheapVenueCountLabel()
+    populateModerateVenueCountLabel()
+    populateExpensiveVenueCountLabel()
   }
+
 }
 
 // MARK: - IBActions
 extension FilterViewController {
-
   @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
 
   }
@@ -62,8 +79,73 @@ extension FilterViewController {
 
 // MARK - UITableViewDelegate
 extension FilterViewController {
-
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
   }
 }
+
+extension FilterViewController {
+  func populateCheapVenueCountLabel() {
+    let fetchRequest = NSFetchRequest<NSNumber>(entityName: "Venue")
+    fetchRequest.resultType = .countResultType
+    fetchRequest.predicate = cheapVenuePredicate
+
+    do {
+      let countResult = try coreDataStack.managedContext.fetch(fetchRequest)
+      let count = countResult.first?.intValue
+      firstPriceCategoryLabel.text = "\(count!) bubble tea places"
+    } catch let error as NSError {
+      print("Count not fetch \(error), Description \(error.userInfo)")
+    }
+  }
+
+  func populateModerateVenueCountLabel() {
+    let fetchRequest = NSFetchRequest<NSNumber>(entityName: "Venue")
+    fetchRequest.resultType = .countResultType
+    fetchRequest.predicate = moderateVenuePredicate
+
+    do {
+      let countResult = try coreDataStack.managedContext.fetch(fetchRequest)
+      let count = countResult.first?.intValue
+      secondPriceCategoryLabel.text = "\(count!) bubble tea places"
+    } catch let error as NSError {
+      print("Count not fetch \(error), Description \(error.userInfo)")
+    }
+  }
+
+  func populateExpensiveVenueCountLabel() {
+    let fetchRequest = NSFetchRequest<NSNumber>(entityName: "Venue")
+    fetchRequest.resultType = .countResultType
+    fetchRequest.predicate = expensiveVenuePredicate
+
+    do {
+      let countResult = try coreDataStack.managedContext.fetch(fetchRequest)
+      let count = countResult.first?.intValue
+      thirdPriceCategoryLabel.text = "\(count!) bubble tea places"
+    } catch let error as NSError {
+      print("Count not fetch \(error), Description \(error.userInfo)")
+    }
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
